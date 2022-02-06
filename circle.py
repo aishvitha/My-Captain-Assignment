bc567d37ee1ffb1cda3d601abb431dff339195b4
aishvitha <97435769+aishvitha@users.noreply.github.com>
2022-02-06 13:52:31 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 31 20:12:09 2022
+
+@author: LENOVO
+"""
+import math
+radius=float(input("enter the radius"))
+area=math.pi*radius*radius
+print("area of the circle is", area)
