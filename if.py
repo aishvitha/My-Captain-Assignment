0fb944cc41e32593f2e0c479a173327370a200e7
aishvitha <97435769+aishvitha@users.noreply.github.com>
2022-02-06 13:52:59 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  1 19:44:57 2022
+
+@author: LENOVO
+"""
+
+#if it rains then i will take umbrella else donot take umbrella
+
+rain=0
+if rain==1:
+    print("umbrella")
+else:
+    print("no umbrella")
+        
+
+    
